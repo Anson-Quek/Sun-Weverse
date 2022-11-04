ca504234d3a88e72fc2f08ba4f3aa184168402b0
Anson-Quek <ansonquekmj@gmail.com>
2022-11-05 00:53:48 +0800

    Changed message_count to be optional.

--- sunverse/objects/live.py
+++ sunverse/objects/live.py
@@ -3,22 +3,26 @@ from sunverse.objects.media import WeverseMedia
 
 class Live(WeverseMedia):
     """Represents a Weverse Live Broadcast.
-    Inherits from :class:`sunverse.objects.WeverseMedia`.
+    Inherits from :class:`objects.media.WeverseMedia`.
 
-    Shares the same attributes with :class:`sunverse.objects.Postlike`,
-    :class:`sunverse.objects.Medialike` and :class:`sunverse.objects.WeverseMedia`.
+    Shares the same attributes with :class:`objects.post.Postlike`,
+    :class:`objects.media.Medialike` and :class:`objects.media.WeverseMedia`.
 
     Attributes
     ----------
-    message_count: :class:`int`
-        The number of messages in the live broadcast.
+    message_count: :class:`int` | :class:`None`
+        The number of messages in the live broadcast, if any.
     """
 
     __slots__ = ("message_count",)
 
     def __init__(self, data: dict):
         super().__init__(data)
-        self.message_count: int = data["extension"]["mediaInfo"]["chat"]["messageCount"]
+        self.message_count: int | None = (
+            data["extension"]["mediaInfo"]["chat"]["messageCount"]
+            if data["extension"]["mediaInfo"].get("chat")
+            else None
+        )
 
     def __repr__(self):
         return f"Live live_id={self.id}, title={self.title}"
